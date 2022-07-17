e96067a82866399b17fc4b0d8773660039afbcec
of3jlmssoo <7a4wiy5vd638@gmail.com>
2022-07-17 21:27:51 +0900

    first commit

---
+++ main.AttributeParser.cpp
@@ -0,0 +1,72 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <sstream>
+using namespace std;
+#include <map>
+#include <string>
+int main() {
+  /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+  int N, Q;
+  string input;
+  string query;
+  // vector<string> tags;
+  vector<string> queries;
+
+  // process the first input. in case of the example " 4 3"
+  getline(cin, input);
+  stringstream ss{input};
+  string times;
+  getline(ss, times, ' ');
+  N = stoi(times);
+  getline(ss, times, ' ');
+  Q = stoi(times);
+
+  // save HRML
+  vector<pair<string, string>> attr_val_vector{};
+  map<std::string, vector<pair<string, string>>> HRML_map;
+  for (auto i = 0; i < N; i++) {
+    attr_val_vector.clear();
+
+    getline(cin, input);
+
+    // ignore the ending tags. assume input for this part is even number
+    if (i < N / 2) {
+      input = input.substr(1, input.size() - 2);  // remove the first and the last charcter
+
+      stringstream sstag{input};
+      string w, tagname, attrname, val;
+      sstag >> tagname;
+      while (sstag >> attrname) {
+        sstag >> w;  // should be the equal sign. ignored
+        sstag >> val;
+        std::pair<string, string> attr_val_pair{attrname, val};
+        attr_val_vector.push_back(attr_val_pair);
+      }
+      // cout << "--> " << input << "--> " << tagname << " & " << attrname << " & " << val << endl;
+      HRML_map[tagname] = attr_val_vector;
+    }
+  }
+
+  // save Queries
+  for (auto i = 0; i < Q; i++) {
+    getline(cin, query);
+    queries.push_back(query);
+  }
+
+  // Process Queries
+  // split a query into two parts, tag part and attr part. ~ is between them
+  // extract the last tag
+  // search the tag with HRML_map
+  // for loop attr_val_vector
+  //  if the attr found in the pari's first field, cout << the value(the second)
+  for (auto q : queries) {
+    stringstream ssq{q};
+    string tagpart, tag, attr_val_part, attr, val;
+    getline(ss, tagpart, '~');
+    getline(ss, attr_val_part, '~');
+  }
+  return 0;
+}
---
+++ readme.md
@@ -0,0 +1,51 @@
+### Attribute Parser
+lex()でToken作って、tokensにpush_backして、その上でparser
+
+<tag-name attribute1-name = "value1" attribute2-name = "value2" ...> あり
+The tags may also be nested.ただ、Examlpleを読むと<tag1 > /n <tag2 >でtag1.tag2.tag3~nameの形態のことを言っているので<tag1 ... tag2>はなさそう
+
+If there are no attributes there is no space after tag name. オープニングタグのみのケースあり
+
+All tag names are unique and the HRML source program is logically correct, i.e. valid nesting.
+A tag can may have no attributes.
+```text
+Sample Input
+
+4 3
+<tag1 value = "HelloWorld">
+<tag2 name = "Name1">
+</tag2>
+</tag1>
+tag1.tag2~name
+tag1~name
+tag1~value
+Sample Output
+
+Name1
+Not Found!
+HelloWorld
+```
+tagはこちら
+<tag-name attribute1-name = "value1" attribute2-name = "value2" ...>
+
+query(example)
+tag1.tag2.tag3~name   "Not Found!"
+tag2にはnameあり。ただ、tag3にはname無し。この場合not foundとなる。
+1. ~で前後分割
+2. 前のタグパートの最後のタグを探す
+3. このタグのvalueに後ろパートがあればその値。なければNot Found
+
+
+pair<string name, string value> attr;
+vector<attr> attrs;
+pair<string tag, vector<atr>> tag;
+vector<tag> tags;
+
+1. check and erase the first charcter and the last character
+2. split with spaces
+   1. the first element is the tag. 
+   2. for loop
+        make the paif of name and value
+        push_back to attrs
+   3. make pair of tag and the vector of attrs
+   4. push_back the pair to the tags
